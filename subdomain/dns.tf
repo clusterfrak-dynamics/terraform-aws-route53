0522512a8cd2fc4860ac4f80267d79ab77b40e87
Kevin Lefevre <lefevre.kevin@gmail.com>
2019-11-05 15:07:45 +0100

    feat: add output for zone_id
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- subdomain/dns.tf
+++ subdomain/dns.tf
@@ -11,3 +11,7 @@ resource "aws_route53_zone" "subdomain" {
 output "route53_subdomain_name_servers" {
   value = aws_route53_zone.subdomain.name_servers
 }
+
+output "route53_subdomain_hosted_zone_id" {
+  value = aws_route53_zone.subdomain.zone_id
+}
