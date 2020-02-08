781bd982bbd0699138783a42aa1278a4249d6f3a
Alban MUSSI <alban_mussi@uptimemanagers.fr>
2020-02-08 08:48:12 +0100

    update: some cosmetic
    Signed-off-by: Alban MUSSI <alban_mussi@uptimemanagers.fr>

--- scripts/tf_checks.sh
+++ scripts/tf_checks.sh
@@ -3,5 +3,5 @@
 find . -type f -name "*.tf" -exec dirname {} \;|sort -u | while read -r dir; do 
     terraform init -backend=false "$dir" || exit 1 
     terraform validate "$dir" || exit 1 
-    terraform fmt -check -diff "$dir" && echo "Directory $(basename $dir) [√]" || exit 1
+    terraform fmt -check -diff "$dir" && echo "[√] $(basename $dir)" || exit 1
 done
